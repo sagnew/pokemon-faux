aa663a9aa9195c6cbad7b3bf84e5e46593b2bb6e
Shahan Akhter <shahan3.14@gmail.com>
2016-07-28 16:35:26 -0400

    edited overlay.py code so that it takes a specified pokemon image and puts it somewhat centered on the image with overlay, also resized the image

--- overlay.py
+++ overlay.py
@@ -1,15 +1,26 @@
+import PIL
 from PIL import Image
 
 
 def overlay(original_image_path):
+    baseheight = 1024 
     background = Image.open(original_image_path)
+    hpercent = (baseheight / float(background.size[1]))
+    wsize = int((float(background.size[0]) * float(hpercent)))
+    background = background.resize((wsize, baseheight), PIL.Image.BILINEAR)
+
     overlay = Image.open('overlay.png')
+    pkmn = Image.open('pokemon-go-images/150.png')
 
     background = background.convert('RGBA')
     overlay = overlay.convert('RGBA')
+    pkmn= pkmn.convert('RGBA')
 
-    overlay = overlay.resize(background.size);
+    overlay = overlay.resize(background.size, PIL.Image.BILINEAR);
 
     new_img = background
     new_img.paste(overlay, (0, 0), overlay)
+
+    new_img.paste(pkmn, (background.size[0]/2, background.size[1]/2), pkmn)
+
     new_img.save(original_image_path,'PNG')
