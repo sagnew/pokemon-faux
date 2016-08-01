372cd0464c0494da8e5fbe2b6a2900c8ab7d1087
Sam Agnew <sagnew@eden.rutgers.edu>
2016-08-01 11:10:09 -0400

    Fix values in overlay.py

--- overlay.py
+++ overlay.py
@@ -2,33 +2,37 @@ import PIL
 from PIL import Image
 
 
-def overlay(original_image_path):
+def overlay(original_image_path, pokemon):
+
+    overlay_image = Image.open('overlay.png')
 
     # This is the image the user sends through text.
     background = Image.open(original_image_path)
 
-    # Resizes the image received so that the height is always 1024px.
-    baseheight = 1024 
-    hpercent = (baseheight / float(background.size[1]))
-    wsize = int((float(background.size[0]) * float(hpercent)))
-    background = background.resize((wsize, baseheight), PIL.Image.BILINEAR)
-
-    overlay = Image.open('overlay.png')
-
-    # Specifies what pokemon sprite is used.
-    pkmn = Image.open('pokemon-go-images/mewtwo.png')
-
-    background = background.convert('RGBA')
-    overlay = overlay.convert('RGBA')
-    pkmn= pkmn.convert('RGBA')
+    # Resizes the image received so that the height is always 512px.
+    base_height = 512
+    height_percent = base_height / background.size[1]
+    width = int(background.size[0] * height_percent)
+    background = background.resize((width, base_height), PIL.Image.BILINEAR)
 
     # Resize the overlay.
-    overlay = overlay.resize(background.size, PIL.Image.BILINEAR);
+    overlay_image = overlay_image.resize(background.size, PIL.Image.BILINEAR)
+
+    # Specify which pokemon sprite is used.
+    pokemon_img = Image.open('pokemon-go-images/{}.png'.format(pokemon))
+
+    # Convert images to RGBA format.
+    background = background.convert('RGBA')
+    overlay_image = overlay_image.convert('RGBA')
+    pokemon_img = pokemon_img.convert('RGBA')
 
     new_img = background
-    new_img.paste(overlay, (0, 0), overlay)
+    new_img.paste(overlay_image, (0, 0), overlay_image)
 
-    # Place the pokemon sprite centered on the background+overlay image.
-    new_img.paste(pkmn, (background.size[0]/30, background.size[1]/9), pkmn)
+    # Place the pokemon sprite centered on the background + overlay image.
+    new_img.paste(pokemon_img,
+                  (int(width / 4), int(base_height / 4)),
+                  pokemon_img)
 
+    # Save the new image.
     new_img.save(original_image_path,'PNG')
